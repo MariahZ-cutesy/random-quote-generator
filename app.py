a5f91ea1ced585f5200846ea0e6ee50cbb56d53c
MariahZ-cutesy <mariahzaidi.work@gmail.com>
2025-09-18 16:20:07 +0800

    Update app.py

--- app.py
+++ app.py
@@ -7,13 +7,6 @@ st.set_page_config(page_title="ZenQuotes Generator", page_icon="🧘", layout="c
 st.title("🧘 Random Zen Quote Generator")
 st.subheader("Get inspired by wisdom from ZenQuotes API")
 
-# Add GIF animation
-st.markdown("""
-    <div style="text-align: center;">
-        https://media.giphy.com/media/3o7aD2saalBwwftBIY/giphy.gif
-    </div>
-""", unsafe_allow_html=True)
-
 def get_quote():
     response = requests.get("https://zenquotes.io/api/random")
     if response.status_code == 200:
